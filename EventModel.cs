b525d750be0b19b184741392ecb4051f51242b33
elenapetkova <74245596+elenapetkova@users.noreply.github.com>
2023-04-26 01:25:47 +0200

    Add files via upload

---
+++ EventModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LABTEST.Models
+{
+    public class EventModel
+    {
+        public int id { get; set; }
+        public string ime { get; set; }
+        [StringLength(30,MinimumLength =5)]
+        public string lokacijon { get; set; }
+    }
+}
