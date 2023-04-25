505e19c9d8d7a6d3c0b8a3dd779d8ca1d72a8510
elenapetkova <74245596+elenapetkova@users.noreply.github.com>
2023-04-26 01:27:22 +0200

    Add files via upload

---
+++ Controllers/EventController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using LABTEST.Models;
+
+namespace LABTEST.Controllers
+{
+    public class EventController : Controller
+    {
+        private static List<EventModel> events = new List<EventModel>();
+
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View(events);
+        }
+
+        // GET: Event/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventModel eventModel = events.FirstOrDefault(e=>e.id==id);
+            if (eventModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventModel);
+        }
+        [HttpPost]
+        public ActionResult Update(EventModel model)
+        {
+            EventModel existingEvent = events.FirstOrDefault(e=>e.id==model.id);
+            if (existingEvent == null)
+            {
+                return HttpNotFound();
+            }
+            existingEvent.ime = model.ime;
+            existingEvent.lokacijon = model.lokacijon;
+            return RedirectToAction("Details", new { id = existingEvent.id });
+        }
+
+        // GET: Event/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Event/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EventModel eventModel)
+        {
+            if (ModelState.IsValid)
+            {
+                events.Add(eventModel);
+                return RedirectToAction("Index");
+            }
+
+            return View(eventModel);
+        }
+
+        // GET: Event/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventModel eventModel = events.FirstOrDefault(e => e.id == id);
+            if (eventModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventModel);
+        }
+
+        // POST: Event/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EventModel eventModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(eventModel);
+        }
+
+        // GET: Event/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventModel eventModel =events.FirstOrDefault(e => e.id == id);
+            if (eventModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventModel);
+        }
+
+        // POST: Event/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EventModel eventModel =events.FirstOrDefault(e => e.id == id);
+            events.Remove(eventModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
